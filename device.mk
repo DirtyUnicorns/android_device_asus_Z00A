e6ba9e254e59af05ad11a31e33a07d36da51a553
jrior001 <jriordan001@gmail.com>
2015-08-18 23:13:12 -0400

    update blobs/ramdisk to 2.20.40.xx sources
    Change-Id: I39f00189f9f1fca58a6de1a05dbda59801c8e606

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ PRODUCT_PACKAGES += \
     config_init.sh \
     fstab \
     fstab.mofd_v1 \
-    fstab.zram \
     init.avc.rc \
     init.bt.rc \
     init.bt.vendor.rc \
@@ -55,7 +54,6 @@ PRODUCT_PACKAGES += \
     init.watchdog.rc \
     init.wifi.rc \
     init.wifi.vendor.rc \
-    init.zram.rc \
     intel_prop \
     intel_prop.cfg \
     rfkill_bt.sh \
